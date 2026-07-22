6478da93f27da255c9f161bb7ee00653fb00d1d5
Romana <www.romanazehra5@gmail.com>
2026-07-22 10:10:07 +0530

    Updated model

--- team3_sentiment_project/sentiment_analysis.py
+++ team3_sentiment_project/sentiment_analysis.py
@@ -1,7 +1,12 @@
+import joblib
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
 import pandas as pd
 
 # Load cleaned dataset
-df = pd.read_csv("data/processed_data/clean_amazon_reviews.csv")
+df = pd.read_csv("team1_data/processed_data/clean_amazon_reviews.csv")
 X = df['clean_review']
 
 # Output (Target)
@@ -12,7 +17,6 @@ print(y.head())
 
 print(df['Sentiment'].value_counts())
 
-from sklearn.feature_extraction.text import TfidfVectorizer
 
 # Convert text into numerical features
 tfidf = TfidfVectorizer()
@@ -21,7 +25,6 @@ X = tfidf.fit_transform(X)
 
 print("Shape of TF-IDF matrix:", X.shape)
 
-from sklearn.model_selection import train_test_split
 
 # Split data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(
@@ -38,7 +41,6 @@ print("Testing Data:", X_test.shape)
     Training the Logistic Regression Model
 ===============================================
 """
-from sklearn.linear_model import LogisticRegression
 
 model = LogisticRegression(max_iter=1000)
 
@@ -48,14 +50,13 @@ print("Model trained successfully!")
 
 """
 ==========================================
-        Step 8: Make Predictions
+        Make Predictions
 ==========================================
 """
 y_pred = model.predict(X_test)
 
 print(y_pred[:10])
 
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
 print("Accuracy:", accuracy_score(y_test, y_pred))
 
@@ -92,9 +93,9 @@ for review, sentiment in zip(reviews, predictions):
     print(f"Review: {review}")
     print(f"Predicted Sentiment: {sentiment}\n")
 
-import joblib
 
 joblib.dump(model, "models/sentiment_model.pkl")
 joblib.dump(tfidf, "models/tfidf_vectorizer.pkl")
 
 print("Model and TF-IDF vectorizer saved successfully!")
+
